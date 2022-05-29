c9bc5a0651d2a5e510ee67cb61b88ad1ee0af4bd
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-29 14:35:37 +0200

    first init

---
+++ iOS-Size Classes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Size Classes
+//
+//  Created by Amjad Oudeh on 29.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
